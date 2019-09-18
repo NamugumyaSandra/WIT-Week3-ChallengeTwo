2ff1f39ec3acaaa36640f6332a66dc2c06d6f9b9
Namugumya Sandra <salyn134@gmail.com>
2019-09-18 21:16:06 +0300

    Add open account method

--- account.js
+++ account.js
@@ -1,25 +1,63 @@
+
 class BankAccount {
-    constructor() {
-        // your code here
+    constructor(owner,balance,status) {
+        this.owner = owner;
+        this.balance = balance;
+        this.minBalance = 10; 
+        this.status = status;
     }
 
-    getBalance() {
-        // your code here
+
+    open(){
+        this.status = 'open';
+        if (this.balance >= this.minBalance){
+            console.log(`Initial Deposit: ${this.balance} USD`);
+        } else{
+            console.log(`Initial deposit should be atleast ${this.minBalance} USD.`)
+        }      
     }
 
-    open() {
-        // your code here
-    }
 
-    deposit() {
-        // your code here
-    }
+    // //transactions
+    // getBalance() {
+    //     console.log(`Balance: ${this.balance} USD`)
+    // };
 
-    withdraw() {
-        // your code here
-    }
+    // deposit(amount) {
+    //     if(this.status === false) {
+    //         console.log('Invalid transaction');
+    //     } else {
+    //         this.balance += amount;
+    //         this.getBalance();
+    //     }
+    // };
 
-    close(){
+    // withdraw(amount) {
+    //     if(this.status === false || amount > this.balance){
+    //         console.log('Invalid transaction');
+    //     }else {
+    //         this.balance -= amount;
+    //         this.getBalance();
+    //     }
+    // };
+    
 
-    }
+    // close(){
+    //     if(this.status === false){
+    //         console.log('You cannot perform any transactions.Account is closed.');
+    //     }else{
+    //         this.open();
+    //     }
+    // }
+    
 }
+
+let newOwner = new BankAccount('asdasfs',3450)
+newOwner.open();
+
+// newOwner.getBalance();
+// newOwner.deposit(2000);
+// newOwner.withdraw(200);
+
+
+module.exports = BankAccount;
